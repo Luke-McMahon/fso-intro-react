50aeb46245d1a6165d1a0c586ea856f6b59d9e6e
Luke McMahon <luke.mcmahon@twobulls.com>
2022-08-16 18:24:02 +1000

    feat: work on destructuring

--- part0/src/App.js
+++ part0/src/App.js
@@ -1,9 +1,14 @@
 const Hello = (props) => {
+  const { name, age } = props;
+
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
@@ -15,6 +20,7 @@ const Footer = () => {
     </div>
   );
 };
+
 const App = () => {
   const name = "Peter";
   const age = 10;
@@ -23,6 +29,7 @@ const App = () => {
     <>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
       <Footer />
     </>
   );
