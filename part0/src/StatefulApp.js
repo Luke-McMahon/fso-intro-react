1618e37bc034f5f0edb413e8122229c37bd15300
Luke McMahon <luke.mcmahon@twobulls.com>
2022-08-17 13:16:35 +1000

    feat: working with state basics

---
+++ part0/src/StatefulApp.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const StatefulApp = () => {
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
+
+  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 });
+
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 });
+
+  return (
+    <>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </>
+  );
+};
+
+export default StatefulApp;
