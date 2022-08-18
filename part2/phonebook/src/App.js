6280505b59ce8813026085ed0ddbae4fa7b8fd48
Luke McMahon <luke.mcmahon@twobulls.com>
2022-08-18 16:13:44 +1000

    feat: b and began working on phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("Greg Daniels");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObj = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObj));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </>
+  );
+};
+
+export default App;
